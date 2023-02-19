e6d9647604cb420f3c282f701c1d1531b0c14ee9
Vấn Nguyễn <vannguyen.cge@gmail.com>
2023-02-19 10:28:44 +0700

    Create randomUniqueHex.js

---
+++ randomUniqueHex.js
@@ -0,0 +1,5 @@
+function randomValueHex(len) {
+    return crypto.randomBytes(Math.ceil(len / 2))
+        .toString('hex') // convert to hexadecimal format
+        .slice(0, len).toUpperCase();   // return required number of characters
+}
