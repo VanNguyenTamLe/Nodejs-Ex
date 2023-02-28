d631adfe8cee76bae850a3b64d0cae6ed6285ab9
Vấn Nguyễn <vannguyen.cge@gmail.com>
2023-02-28 23:25:17 +0700

    Update convertTimeStampMoment.js

--- convertTimeStampMoment.js
+++ convertTimeStampMoment.js
@@ -1,5 +1,10 @@
+const moment = require('moment-timezone');
+
 // đưa ngày giờ hiện tại ở múi giờ +7 về timestamp
 const now = moment().tz('Asia/Bangkok').format('X');
+
+// cộng thêm 28 ngày
+const dateEx = moment.unix(dateCreate).add(28, 'days').unix().toString();
 
 // đưa timestamp về định dạng ngày giờ
 const datetime = moment.unix(now).tz('Asia/Bangkok').format('YYYY-MM-DD HH:mm:ss');
